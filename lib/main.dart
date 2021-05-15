f4745cd9c79bbf64f47326edab0cc21cd8084a0f
gobunny18 <gobunny2020@gmail.com>
2021-05-15 15:51:59 +0530

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Weather',
       theme: ThemeData(primaryColor: Colors.red),
       home: WeatherPage(),
